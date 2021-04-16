b07d56ae0eaa177c0539d24a1fa80addb5fd999f
Annette Rios <arios@ifi.uzh.ch>
2021-04-17 01:17:13 +0200

    add option to give tokenizer text with special tokens (eos + language tags) for multilingual training

--- src/transformers/models/mbart/tokenization_mbart.py
+++ src/transformers/models/mbart/tokenization_mbart.py
@@ -167,6 +167,7 @@ class MBartTokenizer(XLMRobertaTokenizer):
             :obj:`List[int]`: List of `input IDs <../glossary.html#input-ids>`__ with the appropriate special tokens.
         """
         if token_ids_1 is None:
+            print("build input ", self.prefix_tokens + token_ids_0 + self.suffix_tokens)
             return self.prefix_tokens + token_ids_0 + self.suffix_tokens
         # We don't expect to process pairs, but leave the pair logic for API consistency
         return self.prefix_tokens + token_ids_0 + token_ids_1 + self.suffix_tokens
@@ -179,9 +180,13 @@ class MBartTokenizer(XLMRobertaTokenizer):
         tgt_lang: str = "ro_RO",
         **kwargs,
     ) -> BatchEncoding:
-        self.src_lang = src_lang
-        self.tgt_lang = tgt_lang
-        self.set_src_lang_special_tokens(self.src_lang)
+        if kwargs.get("tags_included"):
+            self.src_lang = None
+            self.tgt_lang = None
+        else:
+            self.src_lang = src_lang
+            self.tgt_lang = tgt_lang
+            self.set_src_lang_special_tokens(self.src_lang)
         return super().prepare_seq2seq_batch(src_texts, tgt_texts, **kwargs)
 
     @contextmanager
@@ -196,12 +201,14 @@ class MBartTokenizer(XLMRobertaTokenizer):
 
     def set_src_lang_special_tokens(self, src_lang) -> None:
         """Reset the special tokens to the source lang setting. No prefix and suffix=[eos, src_lang_code]."""
-        self.cur_lang_code = self.lang_code_to_id[src_lang]
-        self.prefix_tokens = []
-        self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
+        if src_lang is not None:
+            self.cur_lang_code = self.lang_code_to_id[src_lang]
+            self.prefix_tokens = []
+            self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
 
     def set_tgt_lang_special_tokens(self, lang: str) -> None:
         """Reset the special tokens to the target language setting. No prefix and suffix=[eos, tgt_lang_code]."""
-        self.cur_lang_code = self.lang_code_to_id[lang]
-        self.prefix_tokens = []
-        self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
+        if lang is not None:
+            self.cur_lang_code = self.lang_code_to_id[lang]
+            self.prefix_tokens = []
+            self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
